22d1fd8aa2e3ba77e956805046e67787ac9eb062
BurnsPaige <paige.burns@laverne.edu>
2026-02-01 10:56:41 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,15 @@
+// Code and Game by Paige Burns
+// Started 1/29/2026
+// Finished TBD
+
 #include "GameStages.h"
 #include "Utils.h"
+#include "Inventory.h"
 
-map<string, bool> playerInventory;
+map<string, Item> playerInventory;
 
 int main() {
+    initInventory();
     start_menu();
     return 0;
 }
