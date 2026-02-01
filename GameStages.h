86c62e8a2450ac74ca9378619ab9daf906dba916
BurnsPaige <paige.burns@laverne.edu>
2026-02-01 10:56:21 -0800

    Update GameStages.h

--- GameStages.h
+++ GameStages.h
@@ -5,7 +5,17 @@ void start_menu();
 void sam_house();
 void computer();
 void call_1();
+
 void funeral_room();
 void funeral_relative();
 void funeral_ava();
 
+void grand_house();
+void grand_bed();
+void grand_attic();
+void grand_basement();
+void grand_main();
+
+void pilot_box();
+void kids_box();
+void rich_box();
